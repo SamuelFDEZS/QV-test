c3d14d6f6a498ea2374e62535b061936d3eab25e
SamuelFDEZS <thggsamuel@gmail.com>
2023-08-10 01:53:15 +0200

    Modified prueba3

--- Prueba 3/script.js
+++ Prueba 3/script.js
@@ -1,6 +1,7 @@
 const numeros = [1,6,8,12,5,67,8,54,75,345]
 
 let sumaPares = 0
+const numero = document.querySelector(".numero")
 
 for (let i = 0; i < numeros.length; i++) {
     if (i % 2 === 0) {
@@ -8,4 +9,4 @@ for (let i = 0; i < numeros.length; i++) {
     }
 }
 
-console.log(sumaPares)
+numero.innerHTML = sumaPares
